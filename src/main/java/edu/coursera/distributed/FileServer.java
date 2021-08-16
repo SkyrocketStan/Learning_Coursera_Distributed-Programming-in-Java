8888efec30b771af6a116253c068cb2a45d79e46
Stanislav Rakitov <prosperousrf@gmail.com>
2021-08-16 12:41:00 +0300

    Week2. Done

--- src/main/java/edu/coursera/distributed/FileServer.java
+++ src/main/java/edu/coursera/distributed/FileServer.java
@@ -1,72 +1,59 @@
 package edu.coursera.distributed;
 
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.File;
-
 /**
- * A basic and very limited implementation of a file server that responds to GET
- * requests from HTTP clients.
+ * A basic and very limited implementation of a file server that responds to GET requests from HTTP
+ * clients.
  */
 public final class FileServer {
-    /**
-     * Main entrypoint for the basic file server.
-     *
-     * @param socket Provided socket to accept connections on.
-     * @param fs A proxy filesystem to serve files from. See the PCDPFilesystem
-     *           class for more detailed documentation of its usage.
-     * @throws IOException If an I/O error is detected on the server. This
-     *                     should be a fatal error, your file server
-     *                     implementation is not expected to ever throw
-     *                     IOExceptions during normal operation.
+  /**
+   * Main entrypoint for the basic file server.
+   *
+   * @param socket Provided socket to accept connections on.
+   * @param fs A proxy filesystem to serve files from. See the PCDPFilesystem class for more
+   *     detailed documentation of its usage.
+   * @throws IOException If an I/O error is detected on the server. This should be a fatal error,
+   *     your file server implementation is not expected to ever throw IOExceptions during normal
+   *     operation.
+   */
+  public void run(final ServerSocket socket, final PCDPFilesystem fs) throws IOException {
+    /*
+     * Enter a spin loop for handling client requests to the provided
+     * ServerSocket object.
      */
-    public void run(final ServerSocket socket, final PCDPFilesystem fs)
-            throws IOException {
-        /*
-         * Enter a spin loop for handling client requests to the provided
-         * ServerSocket object.
-         */
-        while (true) {
+    //noinspection InfiniteLoopStatement
+    while (true) {
+      Socket s = socket.accept();
 
-            // TODO Delete this once you start working on your solution.
-            throw new UnsupportedOperationException();
+      InputStream inputstream = s.getInputStream();
+      InputStreamReader reader = new InputStreamReader(inputstream);
+      BufferedReader bufferedReader = new BufferedReader(reader);
 
-            // TODO 1) Use socket.accept to get a Socket object
+      String line = bufferedReader.readLine();
+      assert line != null;
+      assert line.startsWith("GET");
+      final PCDPPath path = new PCDPPath(line.split(" ")[1]);
+      final String file = fs.readFile(path);
 
-            /*
-             * TODO 2) Using Socket.getInputStream(), parse the received HTTP
-             * packet. In particular, we are interested in confirming this
-             * message is a GET and parsing out the path to the file we are
-             * GETing. Recall that for GET HTTP packets, the first line of the
-             * received packet will look something like:
-             *
-             *     GET /path/to/file HTTP/1.1
-             */
+      OutputStream out = s.getOutputStream();
+      PrintWriter printWriter = new PrintWriter(out);
 
-            /*
-             * TODO 3) Using the parsed path to the target file, construct an
-             * HTTP reply and write it to Socket.getOutputStream(). If the file
-             * exists, the HTTP reply should be formatted as follows:
-             *
-             *   HTTP/1.0 200 OK\r\n
-             *   Server: FileServer\r\n
-             *   \r\n
-             *   FILE CONTENTS HERE\r\n
-             *
-             * If the specified file does not exist, you should return a reply
-             * with an error code 404 Not Found. This reply should be formatted
-             * as:
-             *
-             *   HTTP/1.0 404 Not Found\r\n
-             *   Server: FileServer\r\n
-             *   \r\n
-             *
-             * Don't forget to close the output stream.
-             */
-        }
+      if (file != null) {
+        printWriter.write("HTTP/1.0 200 OK\r\n");
+        printWriter.write("Server: FileServer\r\n");
+        printWriter.write("\r\n");
+        printWriter.write(file + "\r\n");
+      } else {
+        printWriter.write("HTTP/1.0 404 Not Found\r\n");
+        printWriter.write("Server: FileServer\r\n");
+        printWriter.write("\r\n");
+
+      }
+      printWriter.flush();
+      out.close();
     }
+  }
 }
