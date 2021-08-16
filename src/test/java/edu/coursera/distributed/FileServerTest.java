ebefd737ec9a3dec5f111c033af74904dc93cf76
Stanislav Rakitov <prosperousrf@gmail.com>
2021-08-16 13:57:44 +0300

    Week4. Init files

--- src/test/java/edu/coursera/distributed/FileServerTest.java
+++ src/test/java/edu/coursera/distributed/FileServerTest.java
@@ -32,6 +32,19 @@ public class FileServerTest extends TestCase {
 
     private static final Map<String, String> files = new HashMap<String, String>();
 
+    private static int getNCores() {
+        String ncoresStr = System.getenv("COURSERA_GRADER_NCORES");
+        if (ncoresStr == null) {
+            ncoresStr = System.getProperty("COURSERA_GRADER_NCORES");
+        }
+
+        if (ncoresStr == null) {
+            return Runtime.getRuntime().availableProcessors();
+        } else {
+            return Integer.parseInt(ncoresStr);
+        }
+    }
+
     private static String getRandomFileContents(final int len) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < len; i++) {
@@ -60,6 +73,7 @@ public class FileServerTest extends TestCase {
         files.put("/static/dir1/C.txt", getRandomFileContents(10));
         files.put("/static/dir3/dir4/E.txt", getRandomFileContents(10));
         files.put("/static/ABC.txt", getRandomFileContents(2048));
+        files.put("/static/large.txt", getRandomFileContents(1048576));
     }
 
     private PCDPFilesystem getFilesystem() {
@@ -87,7 +101,7 @@ public class FileServerTest extends TestCase {
             public void run() {
                 try {
                     FileServer server = new FileServer();
-                    server.run(socket, fs);
+                    server.run(socket, fs, getNCores());
                 } catch (SocketException s) {
                     // Do nothing, assume killed by main thread
                 } catch (ClosedByInterruptException s) {
@@ -160,7 +174,7 @@ public class FileServerTest extends TestCase {
 
         HttpResponse response = sendHttpRequest(rootDirName + "/A.txt", true);
         assertEquals(200, response.code);
-        assertEquals(files.get("/static/A.txt"), response.body);
+        assertEquals(response.body, files.get("/static/A.txt"));
 
         // Termination
         server.socket.close();
@@ -173,11 +187,11 @@ public class FileServerTest extends TestCase {
 
         HttpResponse response = sendHttpRequest(rootDirName + "/A.txt", true);
         assertEquals(200, response.code);
-        assertEquals(files.get("/static/A.txt"), response.body);
+        assertEquals(response.body, files.get("/static/A.txt"));
 
         response = sendHttpRequest(rootDirName + "/B.txt", true);
         assertEquals(200, response.code);
-        assertEquals(files.get("/static/B.txt"), response.body);
+        assertEquals(response.body, files.get("/static/B.txt"));
 
         // Termination
         server.socket.close();
@@ -190,7 +204,7 @@ public class FileServerTest extends TestCase {
 
         HttpResponse response = sendHttpRequest(rootDirName + "/dir1/C.txt", true);
         assertEquals(200, response.code);
-        assertEquals(files.get("/static/dir1/C.txt"), response.body);
+        assertEquals(response.body, files.get("/static/dir1/C.txt"));
 
         // Termination
         server.socket.close();
@@ -203,7 +217,7 @@ public class FileServerTest extends TestCase {
 
         HttpResponse response = sendHttpRequest(rootDirName + "/dir3/dir4/E.txt", true);
         assertEquals(200, response.code);
-        assertEquals(files.get("/static/dir3/dir4/E.txt"), response.body);
+        assertEquals(response.body, files.get("/static/dir3/dir4/E.txt"));
 
         // Termination
         server.socket.close();
@@ -216,7 +230,7 @@ public class FileServerTest extends TestCase {
 
         HttpResponse response = sendHttpRequest(rootDirName + "/ABC.txt", true);
         assertEquals(200, response.code);
-        assertEquals(files.get("/static/ABC.txt"), response.body);
+        assertEquals(response.body, files.get("/static/ABC.txt"));
 
         // Termination
         server.socket.close();
@@ -248,6 +262,71 @@ public class FileServerTest extends TestCase {
         server.thread.join();
     }
 
+    private int runPerformanceTest(final int nDriverThreads)
+            throws IOException, InterruptedException {
+        final HttpServer server = launchServer();
+
+        Thread[] driverThreads = new Thread[nDriverThreads];
+        RequestDriver[] drivers = new RequestDriver[driverThreads.length];
+
+        for (int i = 0; i < drivers.length; i++) {
+            drivers[i] = new RequestDriver();
+            drivers[i].addRequest(rootDirName + "/large.txt",
+                    files.get("/static/large.txt"));
+            driverThreads[i] = new Thread(drivers[i]);
+        }
+
+        for (int i = 0; i < driverThreads.length; i++) {
+            driverThreads[i].start();
+        }
+
+        Thread.sleep(10000);
+
+        server.socket.close();
+        server.thread.interrupt();
+        server.thread.join();
+
+        int completedRequests = 0;
+        for (int i = 0; i < drivers.length; i++) {
+            completedRequests += drivers[i].getNRequests();
+        }
+        return completedRequests;
+    }
+
+    public void testPerformance() throws IOException, InterruptedException {
+        int nDriverThreads = getNCores();
+
+        System.err.println("Testing performance of multi-threaded web server " +
+                "using " + nDriverThreads + " request threads");
+        final int seqRequests = runPerformanceTest(1);
+        final int parallelRequests = runPerformanceTest(nDriverThreads);
+
+        final double improvement = (double)parallelRequests /
+            (double)seqRequests;
+
+        System.err.println("Single-core execution completed " + seqRequests);
+        System.err.println("Parallel execution completed " +
+                parallelRequests + ", yielding an improvement of " +
+                improvement + "x");
+        System.err.println();
+
+        /*
+         * Expect some parallel improvement, though we don't expect it to scale
+         * perfectly with threads.
+         */
+        final double expected;
+        if (nDriverThreads == 2) {
+            expected = 1.0;
+        } else if (nDriverThreads == 4) {
+            expected = 2.0;
+        } else {
+            expected = 0.6 * nDriverThreads;
+        }
+        final String msg = "Expected parallel threads to produce at least a " +
+            expected + "x improvement, but only saw " + improvement + "x";
+        assertTrue(msg, improvement > expected);
+    }
+
     class HttpResponse {
         public final int code;
         public final String body;
@@ -265,6 +344,41 @@ public class FileServerTest extends TestCase {
         HttpServer(final Thread setThread, final ServerSocket setSocket) {
             thread = setThread;
             socket = setSocket;
+        }
+    }
+
+    class RequestDriver implements Runnable {
+        private final Map<String, String> requests =
+            new HashMap<String, String>();
+        private int nRequests = 0;
+
+        public void addRequest(final String filename, final String body) {
+            requests.put(filename, body);
+        }
+
+        public int getNRequests() {
+            return nRequests;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    for (Map.Entry<String, String> r : requests.entrySet()) {
+                        HttpResponse response = sendHttpRequest(r.getKey(),
+                                false);
+                        assertEquals(response.body, r.getValue());
+                    }
+
+                    nRequests++;
+                }
+            } catch (ConnectException c) {
+                // Is fine, server shut down
+            } catch (SocketException s) {
+                // Is fine, server shut down
+            } catch (IOException io) {
+                throw new RuntimeException(io);
+            }
         }
     }
 }
